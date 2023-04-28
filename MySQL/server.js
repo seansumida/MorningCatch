23b6cb6bf85fa0fcf6f1da7e26fdad2c6ec997ea
joeynhieu <jnhieu@hawaii.edu>
2023-04-28 00:47:12 -1000

    merged changes

--- MySQL/server.js
+++ MySQL/server.js
@@ -15,7 +15,7 @@ var con = mysql.createConnection({
   host: '127.0.0.1',
   user: "root",
   port: 3306,
-  database: "Travel",
+  database: "FreshCatch_DB",
   password: ""
 });
 
@@ -330,7 +330,7 @@ app.get("/checkout", function (request, response) {
 function popular_query(POST, response){
   if(POST['category'] == 'most'){
     category = JSON.stringify(POST['category']);
-    query = "SELECT guestNo, guestName FROM Guest"
+    query = "SELECT * FROM view_most_popular_product"
     con.query(query, function(err, result, fields){
       if (err) throw err;
       console.log(result);
@@ -338,11 +338,11 @@ function popular_query(POST, response){
       var res_json = JSON.parse(res_string);
       console.log(res_json);
       //make table for output
-      response_form = `<form action="popular.html" method="GET">`;
+      response_form = `<form action="../queries/popular.html" method="GET">`;
       response_form += `<table border="3" cellpadding="5" cellspacing="5">`;
       for (i in res_json){
-        response_form += `<tr><td> ${res_json[i].guestNo}</td>`;
-        response_form += `<td> ${res_json[i].guestName}</td>`;
+        response_form += `<tr><td> ${i+1}</td>`;
+        response_form += `<td> ${res_json[i].Product_name}</td>`;
       }
       response_form += "</table>";
       response_form += `<input type="submit" value="Click to Go Back"> </form>`;
@@ -351,7 +351,7 @@ function popular_query(POST, response){
     
   } else if (POST['category'] == 'least'){
     category = JSON.stringify(POST['category']);
-    query = "SELECT * FROM Guest "
+    query = "SELECT * FROM view_most_popular_product"
     con.query(query, function(err, result, fields){
       if (err) throw err;
       console.log(result);
@@ -362,7 +362,8 @@ function popular_query(POST, response){
       response_form = `<form action="/queries/popular.html" method="GET">`;
       response_form += `<table border="3" cellpadding="5" cellspacing="5">`;
       for (i in res_json){
-        response_form += `<tr><td> ${res_json[i].guestAddress}</td>`;
+        response_form += `<tr><td> ${i+1}</td>`;
+        response_form += `<td> ${res_json[i].Product_name}</td>`;
       }
       response_form += "</table>";
       response_form += `<input type="submit" value="Click to Go Back"> </form>`;
@@ -380,7 +381,7 @@ app.post("/popular", function(request, response){
 function customers_query(POST, response){
   if(isNonNegInt(POST['customers_id'])){
     customerId = POST['customers_id'];
-    query = "SELECT * FROM Hotel WHERE hotelNo = " + customerId;
+    query = "SELECT * FROM view_customer_sales_report WHERE P_ID = " + customerId;
     con.query(query, function(err, result, fields){
       if (err) throw err;
       console.log(result);
@@ -391,8 +392,11 @@ function customers_query(POST, response){
       response_form = `<form action="/queries/Customer.html" method="GET">`;
       response_form += `<table border="3" cellpadding="5" cellspacing="5">`;
       for (i in res_json){
-        response_form += `<tr><td> ${res_json[i].hotelNo}</td>`;
-        response_form += `<td> ${res_json[i].hotelName}</td>`;
+        response_form += `<tr><td> ${res_json[i].P_ID}</td>`;
+        response_form += `<td> ${res_json[i].C_Name}</td>`;
+        response_form += `<td> ${res_json[i].Product_Name}</td>`;
+        response_form += `<td> ${res_json[i].Amount_Spent}</td>`;
+        response_form += `<td> ${res_json[i].Visit_amt}</td>`;
       }
       response_form += "</table>";
       response_form += `<input type="submit" value="Click to Go Back"> </form>`;
@@ -427,7 +431,7 @@ function isNonNegInt(stringToCheck, returnErrors = false) {
 function employee_hours_report(POST, response){
   if(isNonNegInt(POST['employee_id'])){
     employeeId = POST['employee_id'];
-    query = "SELECT * FROM Guest WHERE guestNo = " + employeeId;
+    query = "SELECT * FROM view_emp_hours_report WHERE S_id = " + employeeId;
     con.query(query, function(err, result, fields){
       if (err) throw err;
       console.log(result);
@@ -435,12 +439,11 @@ function employee_hours_report(POST, response){
       var res_json = JSON.parse(res_string);
       console.log(res_json);
       //make table for output
-      response_form = `<form action="/reports/Employee.html" method="GET">`;
+      response_form = `<form action="/reports/Employee_hours.html" method="GET">`;
       response_form += `<table border="3" cellpadding="5" cellspacing="5">`;
       for (i in res_json){
-        response_form += `<tr><td> ${res_json[i].guestNo}</td>`;
-        response_form += `<td> ${res_json[i].guestName}</td>`;
-        response_form += `<td> ${res_json[i].guestAddress}</td>`;
+        response_form += `<tr><td> ${res_json[i].Ename}</td>`;
+        response_form += `<td> ${res_json[i].hours_worked}</td>`;
       }
       response_form += "</table>";
       response_form += `<input type="submit" value="Click to Go Back"> </form>`;
@@ -458,7 +461,7 @@ app.post("/employee_hours", function(request, response){
 
 function most_hours_report(POST, response){
   if(POST['most_hours'] != 'undefined'){
-    query = "SELECT * FROM Room ORDER BY roomNo DESC LIMIT 5";
+    query = "SELECT * FROM view_most_worked_employee";
     con.query(query, function(err, result, fields){
       if (err) throw err;
       console.log(result);
@@ -466,11 +469,11 @@ function most_hours_report(POST, response){
       var res_json = JSON.parse(res_string);
       console.log(res_json);
       //make table for output
-      response_form = `<form action="/reports/Employee.html" method="GET">`;
+      response_form = `<form action="/reports/Employee_hours.html" method="GET">`;
       response_form += `<table border="3" cellpadding="5" cellspacing="5">`;
       for (i in res_json){
-        response_form += `<tr><td> ${res_json[i].roomNo}</td>`;
-        response_form += `<td> ${res_json[i].hotelNo}</td>`;
+        response_form += `<tr><td> ${res_json[i].Ename}</td>`;
+        response_form += `<td> ${res_json[i].hours_worked}</td>`;
       }
       response_form += "</table>";
       response_form += `<input type="submit" value="Click to Go Back"> </form>`;
@@ -488,7 +491,7 @@ app.post("/most_hours", function(request, response){
 
 function inventory_report(POST, response){
   if(POST['inventory'] != 'undefined'){
-    query = "SELECT * FROM Booking";
+    query = "SELECT * FROM view_inventory_report";
     con.query(query, function(err, result, fields){
       if (err) throw err;
       console.log(result);
@@ -499,17 +502,17 @@ function inventory_report(POST, response){
       response_form = `<form action="/reports/Inventory.html" method="GET">`;
       response_form += `<table border="3" cellpadding="5" cellspacing="5">`;
       for (i in res_json){
-        response_form += `<tr><td> ${res_json[i].hotelNo}</td>`;
-        response_form += `<td> ${res_json[i].guestNo}</td>`;
-        response_form += `<td> ${res_json[i].dateFrom}</td>`;
-        response_form += `<td> ${res_json[i].dateTo}</td>`;
-        response_form += `<td> ${res_json[i].roomNo}</td>`;
+        response_form += `<tr><td> ${res_json[i].Item_No}</td>`;
+        response_form += `<td> ${res_json[i].Product_name}</td>`;
+        response_form += `<td> ${res_json[i].Supplier_name}</td>`;
+        response_form += `<td> ${res_json[i].Stock_Quantity}</td>`;
+        response_form += `<td> ${res_json[i].Cost_Per_Item}</td>`;
+        response_form += `<td> ${res_json[i].Inv_value}</td>`;
       }
       response_form += "</table>";
       response_form += `<input type="submit" value="Click to Go Back"> </form>`;
       response.send(response_form);
     });
-    
   } else{
     response.send("Try Again");
   }
@@ -521,7 +524,7 @@ app.post("/inventory", function(request, response){
 
 function sales_report(POST, response){
   if(POST['sales'] != 'undefined'){
-    query = "SELECT * FROM Hotel";
+    query = "SELECT * FROM view_sales_report";
     con.query(query, function(err, result, fields){
       if (err) throw err;
       console.log(result);
@@ -532,9 +535,11 @@ function sales_report(POST, response){
       response_form = `<form action="/reports/Sales.html" method="GET">`;
       response_form += `<table border="3" cellpadding="5" cellspacing="5">`;
       for (i in res_json){
-        response_form += `<tr><td> ${res_json[i].hotelNo}</td>`;
-        response_form += `<td> ${res_json[i].hotelName}</td>`;
-        response_form += `<td> ${res_json[i].city}</td>`;
+        response_form += `<tr><td> ${res_json[i].Item_No}</td>`;
+        response_form += `<td> ${res_json[i].Product_name}</td>`;
+        response_form += `<td> ${res_json[i].Price}</td>`;
+        response_form += `<td> ${res_json[i].Quantity}</td>`;
+        response_form += `<td> ${res_json[i].Total_Sales}</td>`;
       }
       response_form += "</table>";
       response_form += `<input type="submit" value="Click to Go Back"> </form>`;
