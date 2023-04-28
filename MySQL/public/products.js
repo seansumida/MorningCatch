187ee2d1e6cf2feb425710f358af3e338b6411df
joeynhieu <jnhieu@hawaii.edu>
2023-04-28 02:23:47 -1000

    updated products.json product.js and product_search

--- MySQL/public/products.js
+++ MySQL/public/products.js
@@ -1,151 +1,101 @@
 products = [
-        {
-            "name" : "Artisan Sanrio: Cinnamoroll",
-            "price" : 25.15,
-            "image" : "https://i.etsystatic.com/14420193/r/il/114385/3606877892/il_1588xN.3606877892_627y.jpg",
-            "stock_avl" : 10
-        },
-        {
-            "name" : "Artisan Studio Ghibli: Totoro",
-            "price" : 22.60,
-            "image" : "https://i.etsystatic.com/14420193/r/il/f16577/3547566335/il_1588xN.3547566335_fnf3.jpg",
-            "stock_avl" : 10
-        },
-        {
-            "name" : "Artisan Pokemon: Cubone",
-            "price" : 50.00,
-            "image" : "https://i.etsystatic.com/26055747/r/il/e14c92/3464508854/il_1588xN.3464508854_7dns.jpg",
-            "stock_avl" : 10
-        },
-        {
-            "name" : "Artisan Pokemon: Swirlix",
-            "price" : 50.00,
-            "image" : "https://i.etsystatic.com/26055747/r/il/9031df/3711736738/il_1588xN.3711736738_fumc.jpg",
-            "stock_avl" : 10
-        },
-        {
-            "name" : "Artisan Green Tea Bear",
-            "price" : 50.00,
-            "image" : "https://i.etsystatic.com/14420193/r/il/a24dc7/3803965181/il_1588xN.3803965181_7a0o.jpg",
-            "stock_avl" : 10
-        },
-        {
+    {
+        "name" : "Spicy Ahi & Shoyu Limu",
+        "price" : 18.00,
+        "image" : "./images/morningcatch/sp_ah_sh_limu.jpg",
+        "info" : "2 Choice Bowl - Combination of Classic Spicy Ahi and Local Shoyu Limu with rice. Sides of kim chee cucumber and nori."
+    },
+    {
+        "name" : "Kim Chee Tako",
+        "price" : 15.00,
+        "image" : "./images/morningcatch/kim_tako.jpg",
+        "info" : "1 Choice Bowl - Kim Chee Tako with rice. Sides of nori."
+    },
+    {
+        "name" : "King Salmon & Hamachi",
+        "price" : 15.00,
+        "image" : "./images/morningcatch/king_sal_ham.jpg",
+        "info" : "2 Choice Bowl - Combination of King Salmon and Hamachi with rice. Sides of kim chee cucumber and nori. <br> Added: Drizzle and Crab Salad"
+    },
+    {
+        "name" : "Spicy King Salmon & Spicy Ahi",
+        "price" : 15.00,
+        "image" : "./images/morningcatch/sp_sal_sp_ahi.jpg",
+        "info" : "2 Choice Bowl - Combination of New Spicy King Salmon and Classic Spicy Ahi with rice. Sides of nori."
+    },
+    {
+        "name" : "Volcano",
+        "price" : 15.00,
+        "image" : "./images/morningcatch/vol_bowl.jpg",
+        "info" : "NEW Bowl - Spicy Ahi and Crab with Drizzle and Tobikko topping."
+    },
+    {
 
-            "name" : "Artisan Mang",
-            "price" : 50.00,
-            "image" : "https://i.etsystatic.com/14420193/r/il/3fe0cd/3654602743/il_1588xN.3654602743_o98m.jpg",
-            "stock_avl" : 10
-        }
-    ,
-        {
-            "name" : "Artisan Floral 1",
-            "price" : 25.15,
-            "image" : "https://i.etsystatic.com/23097501/r/il/c169a6/3617257273/il_1588xN.3617257273_bj66.jpg",
-            "stock_avl" : 10
-        },
-        {
-            "name" : "Artisan Floral 2",
-            "price" : 25.15,
-            "image" : "https://i.etsystatic.com/25641118/r/il/b41242/3832527587/il_1588xN.3832527587_5xbl.jpg",
-            "stock_avl" : 10
-        },
-        {
-            "name" : "Artisan Floral 3",
-            "price" : 25.15,
-            "image" : "https://i.etsystatic.com/25641118/r/il/5693a7/3832536011/il_1588xN.3832536011_1a3k.jpg",
-            "stock_avl" : 10
-        },
-        {
-            "name" : "Artisan Floral 4",
-            "price" : 25.15,
-            "image" : "https://i.etsystatic.com/31503665/r/il/9a56c2/3666248982/il_1588xN.3666248982_ff9o.jpg",
-            "stock_avl" : 10
-        },
-        {
-            "name" : "Artisan Floral 5",
-            "price" : 25.15,
-            "image" : "https://i.etsystatic.com/24746010/r/il/571478/2746964556/il_1588xN.2746964556_u1aq.jpg",
-            "stock_avl" : 10
-        },
-        {
-            "name" : "Artisan Floral 6",
-            "price" : 25.15,
-            "image" : "https://i.etsystatic.com/31129726/r/il/28c663/3909679195/il_1588xN.3909679195_5xow.jpg",
-            "stock_avl" : 10
-        }
+        "name" : "Shoyu Limu",
+        "price" : 15.00,
+        "image" : "./images/morningcatch/sh_limu.jpg",
+        "info" : "1 Choice Bowl - Local Shoyu Limu with rice."
+    }, 
+    {
+
+        "name" : "Hamachi",
+        "price" : 15.00,
+        "image" : "./images/morningcatch/ham.jpg",
+        "info" : "1 Choice Bowl - Hamachi with rice. Sides of kim chee cucumber and nori."
+    },
+    {
+
+        "name" : "Tobikko King Salmon",
+        "price" : 15.00,
+        "image" : "./images/morningcatch/tob_kingsal.jpg",
+        "info" : "1 Choice Bowl - Tobikko King Salmon with rice. Sides of kim chee cucumber and nori."
+    },
+    {
+
+        "name" : "Hawaiian Salt",
+        "price" : 15.00,
+        "image" : "./images/morningcatch/haw_salt.jpg",
+        "info" : "1 Choice Bowl - Hawaiian Salt with rice. Sides of kim chee cucumber and nori."
+    },
+    {
+
+        "name" : "Tobikko Ahi With Drizzle",
+        "price" : 15.00,
+        "image" : "./images/morningcatch/tobahi_driz.jpg",
+        "info" : "1 Choice Bowl - Tobikko Ahi with rice. Sides of kim chee cucumber and nori. <br> Added: Drizzle"
+    },
+    {
+
+        "name" : "Tobikko King Salmon & Shoyu Limu",
+        "price" : 18.00,
+        "image" : "./images/morningcatch/tob_king_sho.jpg",
+        "info" : "2 Choice Bowl - Combination of Tobikko King Salmon and Shoyu Limu with rice. Sides of kim chee cucumber and nori."
+    }
     ,
     {
-        "name" : "Artisan Floral 1",
-        "price" : 25.15,
-        "image" : "https://i.etsystatic.com/23097501/r/il/c169a6/3617257273/il_1588xN.3617257273_bj66.jpg",
-        "stock_avl" : 10
+        "name" : "Spicy Ahi",
+        "price" : 25.00,
+        "image" : "./images/morningcatch/sp_ahi.png",
+        "info" : "Our delicious take on the class Spicy Ahi. <br> Enter the amount of pounds below:"
     },
     {
-        "name" : "Artisan Floral 2",
-        "price" : 25.15,
-        "image" : "https://i.etsystatic.com/25641118/r/il/b41242/3832527587/il_1588xN.3832527587_5xbl.jpg",
-        "stock_avl" : 10
+        "name" : "Shoyu Limu",
+        "price" : 25.00,
+        "image" : "./images/morningcatch/sho_limu.jpg",
+        "info" : "Made on the spot Local Shoyu Limu. <br> Enter the amount of pounds below:"
     },
     {
-        "name" : "Artisan Floral 3",
-        "price" : 25.15,
-        "image" : "https://i.etsystatic.com/25641118/r/il/5693a7/3832536011/il_1588xN.3832536011_1a3k.jpg",
-        "stock_avl" : 10
+        "name" : "Tobikko Shoyu Limu",
+        "price" : 25.00,
+        "image" : "./images/morningcatch/tob_sho_limu.png",
+        "info" : "Our new Tobbiko and Local Shoyu Limu mix. <br> Enter the amount of pounds below:"
     },
     {
-        "name" : "Artisan Floral 4",
-        "price" : 25.15,
-        "image" : "https://i.etsystatic.com/31503665/r/il/9a56c2/3666248982/il_1588xN.3666248982_ff9o.jpg",
-        "stock_avl" : 10
-    },
-    {
-        "name" : "Artisan Floral 5",
-        "price" : 25.15,
-        "image" : "https://i.etsystatic.com/24746010/r/il/571478/2746964556/il_1588xN.2746964556_u1aq.jpg",
-        "stock_avl" : 10
-    },
-    {
-        "name" : "Artisan Floral 6",
-        "price" : 25.15,
-        "image" : "https://i.etsystatic.com/31129726/r/il/28c663/3909679195/il_1588xN.3909679195_5xow.jpg",
-        "stock_avl" : 10
-    },
 
-    {
-        "name" : "Blue Keycap Set",
-        "price" : 25.15,
-        "image" : "https://i.etsystatic.com/23346058/r/il/ec5d67/3766668341/il_1588xN.3766668341_l9tj.jpg",
-        "stock_avl" : 10
-    },
-    {
-        "name" : "Yellow Keycap Set",
-        "price" : 25.15,
-        "image" : "https://i.etsystatic.com/33263928/r/il/6c7c15/3894062635/il_1588xN.3894062635_387e.jpg",
-        "stock_avl" : 10
-    },
-    {
-        "name" : "Crystal Keycap Set",
-        "price" : 25.15,
-        "image" : "https://i.etsystatic.com/31503665/r/il/1ccc3d/3713860269/il_1588xN.3713860269_lojl.jpg",
-        "stock_avl" : 10
-    },
-    {
-        "name" : "Pudding Keycap Set",
-        "price" : 25.15,
-        "image" : "https://i.etsystatic.com/33489200/r/il/d5db6d/3728196027/il_1588xN.3728196027_ej3n.jpg",
-        "stock_avl" : 10
-    },
-    {
-        "name" : "Multi-Color Keycap Set",
-        "price" : 25.15,
-        "image" : "https://i.etsystatic.com/33466748/r/il/11f6b6/3735540428/il_1588xN.3735540428_npbs.jpg",
-        "stock_avl" : 10
-    },
-    {
-        "name" : "Themed Keycap Set: Rabbit",
-        "price" : 25.15,
-        "image" : "https://i.etsystatic.com/23346058/r/il/b4f9cf/3332191355/il_1588xN.3332191355_iuy8.jpg",
-        "stock_avl" : 10
+        "name" : "Tobikko King Salmon",
+        "price" : 25.00,
+        "image" : "./images/morningcatch/tob_king_sal.jpg",
+        "info" : "Our try on Tobbiko and King Salmon Mix. <br> Enter the amount of pounds below:"
     }
 ]
 
